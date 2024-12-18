dc6b2fd05f4b1f0b74e0ac1ada19486c6afd85b9
ROSHAN-PATIL903 <patilroshan8144@gmail.com>
2024-12-18 13:07:00 +0000

    new

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1232131
+let accountEmail="patilroshan@gmail.com"
+var accountPassword="12345"
+accouncity="suart"
+let accountstate;
+
+console.log(accountId);
+
+accountEmail = "hd@ds.com"
+accountPassword= "132323"
+accouncity="vadodara"
+
+console.table([accountId,accountEmail,accountPassword,accouncity,accountstate]);
+/*
+not use the var in defining the variable
+besuse issue in block scop
+*/
